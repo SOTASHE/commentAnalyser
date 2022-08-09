d2a833911d1d2c3665ca3d3781189499a3f6bb87
Snaye Sotashe <sinayesotatshe@gmail.com>
2022-08-09 05:32:38 +0200

    task 3 initiated, used threads to read through all the files

--- src/App.java
+++ src/App.java
@@ -1,24 +1,44 @@
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        File file = new File("docs"); // create a new file object with the path to the directory we want to analyzed
-        File[] files = file.listFiles(); // create an array of files from the directory we want to analyze
-        for (File f : files) { // loop through the files in the directory
-            if (f.getName().endsWith(".txt")) { // if the file is a text file (.txt) we want to analyze we create a new CommentAnalyzer object with the file we want to analyze
-                AnalyzeComments(new Comments(f));
-                 AnalyzeQuestions(new Questions(f));
-                AnalyzeSpam(new Spam(f));
+        //use threads to read the files concurrently and analyze them in parallel using multiple Threads_lock classes and a thread po
+
+        //   process them concurrently using separate threads (ie one thread per file) and then consolidate the results to print the report.
+        //         The current code processes the files sequentially. Change the implementation to process them concurrently using separate threads (ie one thread per file) and then consolidate the results to print the report.  
+        // call all the classes that implements the analyse method and pass the file as an argument to the constructor of the classes
+        // print the results of the analysis in the console output stream using the print method of the Map class and the toString method of the Integer classes
+
+        // code
+        // read through all the files in docs directory using threads and process them concurrently using separate threads (ie one thread per file) and then consolidate the results to print the report.
+        //         The current code processes the files sequentially. Change the implementation to process them concurrently using separate threads (ie one thread per file) and then consolidate the results to print the report.
+        // call all the classes that implements the analyse method and pass the file as an argument to the constructor of the classes
+        // print the results of the analysis in the console output stream using the print method of the Map class and the toString method of the Integer Classes
+
+        // code
+
+        // read through all the files in docs directory using threads and process them concurrently using separate threads (ie one thread per file) and then consolidate the results to print the report.
+	Map<String, Integer> totalResults = new HashMap<>();
+    //Get the list of all files in Array.
+        
+        File[] files = new File("docs").listFiles();
+        for (File file : files) {
+            if (file.isFile()) {
+                System.out.println("Processing file: " + file.getName());
+                  AnalyzeComments(new Comments(file));
+                 AnalyzeQuestions(new Questions(file));
+                AnalyzeSpam(new Spam(file));
                 Comments analyzerComments;
                 Questions analyzerqusetions;
                 Spam analyzerspam;
                   
 
-                analyzerComments = new Comments(f);
-                analyzerqusetions = new Questions(f);
-                analyzerspam = new Spam(f);
+                analyzerComments = new Comments(file);
+                analyzerqusetions = new Questions(file);
+                analyzerspam = new Spam(file);
                 //print the results of the analysis
                 for (Map.Entry<String, Integer> entry : analyzerComments.analyze().entrySet()) {
                     System.out.println(entry.getKey() + ": " + entry.getValue());
@@ -29,10 +49,29 @@ public class App {
                 for (Map.Entry<String, Integer> entry : analyzerspam.analyze().entrySet()) {
                     System.out.println(entry.getKey() + ": " + entry.getValue());
                 }
+
+                Thread_lock thread_lock = new Thread_lock(file, totalResults);
+                thread_lock.start();
             }
         }
+        // wait for all the threads to finish
+        for (Thread_lock thread_lock : Thread_lock.threads) {
+            thread_lock.join();
+        }
+        // consolidate the results to print the report.
+        for (Thread_lock thread_lock : Thread_lock.threads) {
+            totalResults.putAll(thread_lock.results);
+        }
+        // print the results of the analysis in the console output stream using the print method of the Map class and the toString method of the Integer Classes
+        System.out.println(totalResults);
+                 
+
+
+
+
     }
-    
+
+
     private static void AnalyzeComments(Comments comments) {
     }
 
@@ -48,4 +87,4 @@ public class App {
 
 
     }
-}
+    }
---
+++ src/ThreadPool.java
@@ -0,0 +1,13 @@
+
+public class ThreadPool {
+
+    public ThreadPool(int i) {
+    }
+
+    public void submit(Thread_lock thread_lock) {
+    }
+
+    public void shutdown() {
+    }
+
+}
